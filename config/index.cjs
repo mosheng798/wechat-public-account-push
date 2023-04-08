5b8bfb0674f69677fbe7fb401b3e2eedb0851646
mosheng798 <111725560+mosheng798@users.noreply.github.com>
2023-04-08 13:05:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '昭惠',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oI-5y6gyxHbNhZ1LjdTQvpmjITi4',
+      id: 'oI-5y6mGmqEam2EiufV26iQsQ5p8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'LThESeTiTG_HXHUb78wpeLoT9gG4rSKyo9_fWY0zPAE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
