6591cd557fd240d2f853689ff8597586db4ffdd8
mosheng798 <111725560+mosheng798@users.noreply.github.com>
2023-04-08 12:48:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,9 +28,7 @@ const USER_CONFIG = {
       horoscopeDate: '10-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝宝', year: '2003', date: '10-23',
